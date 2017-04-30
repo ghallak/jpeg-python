d719afcb0c8effa39f2dc30cac320071654e3229
Gaith Hallak <gaithhallak@gmail.com>
2017-04-30 21:45:54 +0300

    implemented quantization function

--- encoder.py
+++ encoder.py
@@ -26,6 +26,34 @@ def dct(block):
 
     return trans
 
+def quantize(block, component):
+    # quantization tables are taken from the original JPEG standard
+    # (https://www.w3.org/Graphics/JPEG/itu-t81.pdf) page: 143
+    if component == 'lum':
+        q = np.array([[16, 11, 10, 16, 24,  40,  51,  61 ],
+                      [12, 12, 14, 19, 26,  58,  60,  55 ],
+                      [14, 13, 16, 24, 40,  57,  69,  56 ],
+                      [14, 17, 22, 29, 51,  87,  80,  62 ],
+                      [18, 22, 37, 56, 68,  109, 103, 77 ],
+                      [24, 35, 55, 64, 81,  104, 113, 92 ],
+                      [49, 64, 78, 87, 103, 121, 120, 101],
+                      [72, 92, 95, 98, 112, 100, 103, 99 ]])
+    elif component == 'chrom':
+        q = np.array([[17, 18, 24, 47, 99, 99, 99, 99],
+                      [18, 21, 26, 66, 99, 99, 99, 99],
+                      [24, 26, 56, 99, 99, 99, 99, 99],
+                      [47, 66, 99, 99, 99, 99, 99, 99],
+                      [99, 99, 99, 99, 99, 99, 99, 99],
+                      [99, 99, 99, 99, 99, 99, 99, 99],
+                      [99, 99, 99, 99, 99, 99, 99, 99],
+                      [99, 99, 99, 99, 99, 99, 99, 99]])
+    else:
+        raise ValueError((
+            "component should be either 'lum' or 'chrom', "
+            "but '{comp}' was found").format(comp = component))
+
+    return (block / q).round().astype(np.int8)
+
 def zigzag(block):
     # constants for directions
     UP, DOWN, RIGHT, LEFT, UP_RIGHT, DOWN_LEFT = range(6)
