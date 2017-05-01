58dcbe417f9755e4398d8d77a9ba786ee2efcaf4
Gaith Hallak <gaithhallak@gmail.com>
2017-05-01 06:12:12 +0300

    main function implementation

--- encoder.py
+++ encoder.py
@@ -1,30 +1,9 @@
-import numpy as np
 import math
+import numpy as np
+from scipy import fftpack
+from PIL import Image
+from huffman import HuffmanTree
 
-def dct(block):
-    def block_cos(u, v):
-        def cos_expr(xy, uv):
-            return math.cos((2 * xy + 1) * uv * math.pi / 16)
-
-        res = 0.0
-        for x in range(rows):
-            for y in range(cols):
-                res += block[x, y] * cos_expr(x, u) * cos_expr(y, v)
-        return res
-
-    def alpha(n):
-        return 1 / math.sqrt(2) if n == 0 else 1
-
-    rows, cols = block.shape
-
-    # transformed matrix
-    trans = np.empty((rows, cols), np.float64)
-
-    for u in range(rows):
-        for v in range(cols):
-            trans[u, v] = alpha(u) * alpha(v) * block_cos(u, v) / 4
-
-    return trans
 
 def quantize(block, component):
     # quantization tables are taken from the original JPEG standard
@@ -154,3 +133,88 @@ def run_length_encode(arr):
             values.append(binary_str(elem))
             run_length = 0
     return symbols, values
+
+
+def write_to_file(dc, ac, blocks_count, tables, filename='image.dat'):
+    f = open('data/' + filename, 'w')
+    for b in range(blocks_count):
+        for c in range(3):
+            dc_code = bits_required(dc[b, c])
+            symbols, values = run_length_encode(ac[b, :, c])
+
+            if c == 0:
+                dc_table = tables['dc_y']
+                ac_table = tables['ac_y']
+            else:
+                dc_table = tables['dc_c']
+                ac_table = tables['ac_c']
+
+            f.write(dc_table[dc_code])
+            for i in range(len(symbols)):
+                f.write(ac_table[tuple(symbols[i])])
+                if symbols[i][1] != 0:
+                    f.write(values[i])
+    f.close()
+
+
+def main():
+    image = Image.open('data/lena.tif')
+    ycbcr = image.convert('YCbCr')
+
+    npmat = np.array(ycbcr, dtype=np.int16)
+
+    rows, cols = npmat.shape[0], npmat.shape[1]
+
+    # block size: 8x8
+    if rows % 8 == cols % 8 == 0:
+        blocks_count = rows // 8 * cols // 8
+    else:
+        raise ValueError(("the width and height of the image "
+                          "should both be mutiples of 8"))
+
+    # dc is the top-left cell of the block, ac are all the other cells
+    dc = np.empty((blocks_count, 3), dtype=np.int16)
+    ac = np.empty((blocks_count, 63, 3), dtype=np.int16)
+
+    for i in range(0, rows, 8):
+        for j in range(0, cols, 8):
+            try:
+                block_index += 1
+            except NameError:
+                block_index = 0
+
+            for k in range(3):
+                # split 8x8 block and center the data range on zero
+                # [0, 255] --> [-128, 127]
+                block = npmat[i:i+8, j:j+8, k] - 128
+
+                dct_matrix = fftpack.dct(block, norm='ortho')
+                quant_matrix = quantize(dct_matrix,
+                                        'lum' if k == 0 else 'chrom')
+                zz = zigzag(quant_matrix)
+
+                dc[block_index, k] = zz[0]
+                ac[block_index, :, k] = zz[1:]
+
+    def flatten(l):
+        return [item for sublist in l for item in sublist]
+
+    H_DC_Y = HuffmanTree(np.vectorize(bits_required)(dc[:, 0]))
+    H_DC_C = HuffmanTree(np.vectorize(bits_required)(dc[:, 1:].flat))
+    H_AC_Y = HuffmanTree(
+            flatten(run_length_encode(ac[i, :, 0])[0]
+                    for i in range(blocks_count)))
+    H_AC_C = HuffmanTree(
+            flatten(run_length_encode(ac[i, :, j])[0]
+                    for i in range(blocks_count) for j in [1, 2]))
+
+    tables = {'dc_y': H_DC_Y.value_to_bitstring_table(),
+              'ac_y': H_AC_Y.value_to_bitstring_table(),
+              'dc_c': H_DC_C.value_to_bitstring_table(),
+              'ac_c': H_AC_C.value_to_bitstring_table()}
+
+    write_to_file(dc, ac, blocks_count, tables)
+
+
+if __name__ == "__main__":
+    main()
