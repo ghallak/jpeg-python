59df4f1506973da1f8a7f8c39cc65b4874f71918
Gaith Hallak <gaithhallak@gmail.com>
2017-05-01 04:04:53 +0300

    implemented huffman tree class

---
+++ huffman.py
@@ -0,0 +1,94 @@
+from queue import PriorityQueue
+
+
+class HuffmanTree:
+
+    class __Node:
+        def __init__(self, value, freq, left_child, right_child):
+            self.value = value
+            self.freq = freq
+            self.left_child = left_child
+            self.right_child = right_child
+
+        @classmethod
+        def init_leaf(self, value, freq):
+            return self(value, freq, None, None)
+
+        @classmethod
+        def init_node(self, left_child, right_child):
+            freq = left_child.freq + right_child.freq
+            return self(None, freq, left_child, right_child)
+
+        def is_leaf(self):
+            return self.value is not None
+
+        def __eq__(self, other):
+            stup = self.value, self.freq, self.left_child, self.right_child
+            otup = other.value, other.freq, other.left_child, other.right_child
+            return stup == otup
+
+        def __nq__(self, other):
+            return not (self == other)
+
+        def __lt__(self, other):
+            return self.freq < other.freq
+
+        def __le__(self, other):
+            return self.freq < other.freq or self.freq == other.freq
+
+        def __gt__(self, other):
+            return not (self <= other)
+
+        def __ge__(self, other):
+            return not (self < other)
+
+    def __init__(self, arr):
+        q = PriorityQueue()
+
+        # calculate frequencies and insert them into a priority queue
+        for val, freq in self.__calc_freq(arr).items():
+            q.put(self.__Node.init_leaf(val, freq))
+
+        while q.qsize() >= 2:
+            u = q.get()
+            v = q.get()
+
+            q.put(self.__Node.init_node(u, v))
+
+        self.__root = q.get()
+
+        # dictionaries to store huffman table
+        self.__value_to_bitstring = dict()
+        self.__bitstring_to_value = dict()
+
+    def value_to_bitstring_table(self):
+        if len(self.__value_to_bitstring.keys()) == 0:
+            self.__create_huffman_table()
+        return self.__value_to_bitstring
+
+    def bitstring_to_value_table(self):
+        if len(self.__bitstring_to_value.keys()) == 0:
+            self.__create_huffman_table()
+        return self.__bitstring_to_value
+
+    def __create_huffman_table(self):
+        def tree_traverse(current_node, bitstring=''):
+            if current_node is None:
+                return
+            if current_node.is_leaf():
+                self.__value_to_bitstring[current_node.value] = bitstring
+                self.__bitstring_to_value[bitstring] = current_node.value
+                return
+            tree_traverse(current_node.left_child, bitstring + '0')
+            tree_traverse(current_node.right_child, bitstring + '1')
+
+        tree_traverse(self.__root)
+
+    def __calc_freq(self, arr):
+        freq_dict = dict()
+        for elem in arr:
+            if elem in freq_dict:
+                freq_dict[elem] += 1
+            else:
+                freq_dict[elem] = 1
+        return freq_dict
