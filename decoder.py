953f58c1fc5623c96175566d1021dbaf6ddf6c97
Gaith Hallak <gaithhallak@gmail.com>
2017-05-02 11:42:53 +0300

    implemented image file reading for the decoder

---
+++ decoder.py
@@ -0,0 +1,121 @@
+import numpy as np
+from utils import *
+
+
+class JPEGFileReader:
+    TABLE_SIZE_BITS = 16
+    BLOCKS_COUNT_BITS = 32
+
+    DC_CODE_LENGTH_BITS = 4
+    CATEGORY_BITS = 4
+
+    AC_CODE_LENGTH_BITS = 8
+    RUN_LENGTH_BITS = 4
+    SIZE_BITS = 4
+
+    def __init__(self, filename):
+        self.__file = open(filename, 'r')
+
+    def read_int(self, size):
+        if size == 0:
+            return 0
+
+        bin_num = self.__read_str(size)
+        if bin_num[0] == '1':
+            return self.__int2(bin_num)
+        else:
+            return self.__int2(binstr_flip(bin_num)) * -1
+
+    def read_dc_table(self):
+        table = dict()
+
+        table_size = self.__read_uint(self.TABLE_SIZE_BITS)
+        for _ in range(table_size):
+            category = self.__read_uint(self.CATEGORY_BITS)
+            code_length = self.__read_uint(self.DC_CODE_LENGTH_BITS)
+            code = self.__read_str(code_length)
+            table[code] = category
+        return table
+
+    def read_ac_table(self):
+        table = dict()
+
+        table_size = self.__read_uint(self.TABLE_SIZE_BITS)
+        for _ in range(table_size):
+            run_length = self.__read_uint(self.RUN_LENGTH_BITS)
+            size = self.__read_uint(self.SIZE_BITS)
+            code_length = self.__read_uint(self.AC_CODE_LENGTH_BITS)
+            code = self.__read_str(code_length)
+            table[code] = (run_length, size)
+        return table
+
+    def read_blocks_count(self):
+        return self.__read_uint(self.BLOCKS_COUNT_BITS)
+
+    def read_huffman_code(self, table):
+        prefix = ''
+        # TODO: break the loop if __read_char is not returing new char
+        while prefix not in table:
+            prefix += self.__read_char()
+        return table[prefix]
+
+    def __read_uint(self, size):
+        if size <= 0:
+            raise ValueError("size of unsigned int should be greater than 0")
+        return self.__int2(self.__read_str(size))
+
+    def __read_str(self, length):
+        return self.__file.read(length)
+
+    def __read_char(self):
+        return self.__read_str(1)
+
+    def __int2(self, bin_num):
+        return int(bin_num, 2)
+
+
+def read_image_file(filename):
+    reader = JPEGFileReader(filename)
+
+    tables = dict()
+    for table_name in ['dc_y', 'ac_y', 'dc_c', 'ac_c']:
+        if 'dc' in table_name:
+            tables[table_name] = reader.read_dc_table()
+        else:
+            tables[table_name] = reader.read_ac_table()
+
+    blocks_count = reader.read_blocks_count()
+
+    dc = np.empty((blocks_count, 3), dtype=np.int32)
+    ac = np.empty((blocks_count, 63, 3), dtype=np.int32)
+
+    for block_index in range(blocks_count):
+        for component in range(3):
+            dc_table = tables['dc_y'] if component == 0 else tables['dc_c']
+            ac_table = tables['ac_y'] if component == 0 else tables['ac_c']
+
+            category = reader.read_huffman_code(dc_table)
+            dc[block_index, component] = reader.read_int(category)
+
+            cells_count = 0
+
+            # TODO: try to make reading AC coefficients better
+            while cells_count < 63:
+                run_length, size = reader.read_huffman_code(ac_table)
+
+                if (run_length, size) == (0, 0):
+                    while cells_count < 63:
+                        ac[block_index, cells_count, component] = 0
+                        cells_count += 1
+                else:
+                    for i in range(run_length):
+                        ac[block_index, cells_count, component] = 0
+                        cells_count += 1
+                    if size == 0:
+                        ac[block_index, cells_count, component] = 0
+                    else:
+                        value = reader.read_int(size)
+                        ac[block_index, cells_count, component] = value
+                    cells_count += 1
+
+    return dc, ac, tables
